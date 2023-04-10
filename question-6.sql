7cdb7db842ba086e01bcb50951d079679256f6b5
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-10 15:57:02 -0700

    Update question-6.sql

--- question-6.sql
+++ question-6.sql
@@ -1,1 +1,3 @@
 -- Find tracks (id, name and composer) that are part of a line in an invoice.
+SELECT tracks.TrackId AS id, tracks.Name AS name, tracks.Composer AS composer FROM tracks 
+INNER JOIN invoice_items ON tracks.TrackId = invoice_items.TrackId;
