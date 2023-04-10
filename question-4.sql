5cebdbaf75d18cafd6b6915691d4b7fe1bed3922
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-10 15:34:50 -0700

    Update question-4.sql

--- question-4.sql
+++ question-4.sql
@@ -3,7 +3,7 @@ SELECT employees.FirstName || '' || employees.LastName AS sale_agent,
 SUM(invoices.Total) AS total_sales FROM employees 
 INNER JOIN customers ON employees.EmployeeId = customers.SupportRepId 
 INNER JOIN invoices ON customers.CustomerId = invoices.CustomerId 
-WHERE strftime('%Y', invoices.InvoiceDate) = '2012' 
+WHERE strftime('%Y', invoices.InvoiceDate) = '2010' 
 GROUP BY employees.EmployeeId 
 ORDER BY total_sales DESC;
 -- Thus, MargaretPark made the most in sales in 2010
