f4e1288dee070ebe13eaad7eef74a69da96197c3
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-10 19:40:21 -0700

    Update question-3.sql

--- question-3.sql
+++ question-3.sql
@@ -1,5 +1,5 @@
 -- Provide a query that includes the purchased track name AND artist name with each invoice line item.
-SELECT tracks.Name AS track_name, artists.Name AS artist_name, invoice_items.InvoiceLineId 
+SELECT tracks.Name AS track_name, artists.Name AS artist_name, invoice_items.* 
 FROM invoice_items 
 INNER JOIN tracks ON invoice_items.TrackId = tracks.TrackId 
 INNER JOIN albums ON tracks.AlbumId = albums.AlbumId 
