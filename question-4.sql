79679963497a0ee1e66d94b73c556e2565d09e05
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-11 17:47:49 -0700

    Update question-4.sql

--- question-4.sql
+++ question-4.sql
@@ -1,5 +1,5 @@
 -- Which sales agent made the most in sales in 2010?
-SELECT employees.FirstName || '' || employees.LastName AS sale_agent, 
+SELECT employees.FirstName || ' ' || employees.LastName AS sale_agent, 
 SUM(invoices.Total) AS total_sales FROM employees 
 INNER JOIN customers ON employees.EmployeeId = customers.SupportRepId 
 INNER JOIN invoices ON customers.CustomerId = invoices.CustomerId 
