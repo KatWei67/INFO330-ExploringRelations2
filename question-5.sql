f813fe3e1d728bd66208f8c62261f670d9419222
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-11 17:23:42 -0700

    Update question-5.sql

--- question-5.sql
+++ question-5.sql
@@ -1,9 +1,7 @@
 -- What was the most purchased track of 2013?
-SELECT tracks.Name, SUM(tracks.Name) AS total_purchased FROM tracks 
+SELECT tracks.Name, COUNT(*) AS total_purchased FROM tracks 
 INNER JOIN invoice_items ON tracks.TrackId = invoice_items.TrackId 
 INNER JOIN invoices ON invoices.InvoiceId = invoice_items.InvoiceId
 WHERE strftime('%Y', invoices.InvoiceDate) = '2013'
 GROUP BY tracks.TrackId 
 ORDER BY total_purchased DESC
-LIMIT 5;
---14 Years was the most purchased track of 2013.
